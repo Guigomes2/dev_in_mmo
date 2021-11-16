00fc2dd92bae64f96e489ac0dc4153f2fae350db
Guilherme Gomes <90911760+Guigomes2@users.noreply.github.com>
2021-11-16 14:07:54 -0300

    Início

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Inputs from './componets/Inputs';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const {text, setName} = useState("");
+
+  // useEffect(() => {
+  // if (text) {
+  // fetch('')
+  // .then(res => res.json())
+  // }
+  //}, [text]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <label>O que você procura?   
+      <input
+        type="text"
+        value={text}
+        onChange={setName}
+      />
+    </label>
   );
 }
-
 export default App;
---
+++ src/componets/Inputs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Inputs({ value, setName }) {
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={value}
+                onChange={e => setName(e.target.value)}
+            />
+        </div>
+    )
+}
+
+export default Inputs;
